c2c2d6ab0305681a09ac25229b5f1468bf3a69bf
Michaelkh20 <mukhoollgm@edu.hse.ru>
2023-07-09 19:27:19 +0300

    Structure fixed

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public/dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/',
+    publicPath: '/public/dist/',
   },
   devServer: {
     contentBase: './dist',
